5aff45aea8e54873c7f9c37f6839768aef4be09b
CJ Waisath <38671255+Waisath-CJ@users.noreply.github.com>
2018-11-22 00:49:04 -0700

    JS Basics Updates

--- jsBasics/jsBasics.js
+++ jsBasics/jsBasics.js
@@ -31,7 +31,53 @@ function multiples() {
 }
 
 /////////////////////////////////////////////////////////////////////////////////
+function array() {
+	var arrayMax = parseInt(prompt("How many numbers would you like in your array?"));
 
-function promptArray() {
+	if (isNaN(arrayMax)) {
+		document.getElementById("arrayOutput").innerHTML = "Error: Invalid input. Please try again";
+	} else {
+		var list = createArray(arrayMax);
+	}
+
+	var sortArray = sort(list);
+	var sumValue = addEnds(list);
+	var midValue = getMiddle(list);
+	var range = getRange(list);
+
+	document.getElementById("arrayOutput").innerHTML = "Array of values: " + "[" + list + "]";
+	document.getElementById("sumOutput").innerHTML = "Sum of the first and last values: " + sumValue;
+	document.getElementById("midOutput").innerHTML = "Middle integer: " + midValue;
+	document.getElementById("rangeOutput").innerHTML = "Range of values: " + range;
+}
+
+function createArray(arrayMax){
+	var i = 0;
+	var list = [];
+
+	do {
+		var inputArray = parseFloat(prompt("Enter an integer."));
+
+		if (isNaN(inputArray)) {
+			break;
+		} else {
+			list.push(inputArray);
+			i++;
+		}
+	} while (i < arrayMax);
+
+	return list;
+}
+
+function addEnds(list) {
+	var firstValue = list[0];
+	var lastValue = list[list.length - 1];
+	
+	var sum = firstValue + lastValue;
+
+	return sum;
+}
+
+function getMiddle(list) {
 	
 }

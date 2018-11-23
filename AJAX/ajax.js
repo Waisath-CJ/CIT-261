343a3b1d88ab65638598fcee901275dbadebb50d
CJ Waisath <38671255+Waisath-CJ@users.noreply.github.com>
2018-11-23 09:45:14 -0700

    Update ajax.js

--- AJAX/ajax.js
+++ AJAX/ajax.js
@@ -4,7 +4,7 @@ function familyJSON(value) {
 	var xhttp = new XMLHttpRequest();
 	
 	xhttp.onreadystatechange = function() {
-		if (this.readystate == 4 && this.status == 200) {
+		if (this.readyState == 4 && this.status == 200) {
 			var response = JSON.parse(this.responseText);
 
 			var image = '<img src="' + response[value].pic + '">';
